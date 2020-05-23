61c62907222b9429ac686e50e8317c0ddc5d7f5d
Mark B DSouza <mark.benjamin.dsouza@gmail.com>
2020-05-23 20:30:01 +0530

    mini project 3 added

--- Mini-Projects/Etch a Sketch/etch-a-sketch.js
+++ Mini-Projects/Etch a Sketch/etch-a-sketch.js
@@ -1,6 +1,6 @@
-const canvas = document.querySelector("#etch-a-sketch");
-const ctx = canvas.getContext("2d");
-const shakeBtn = document.querySelector(".shake");
+const canvas = document.querySelector('#etch-a-sketch');
+const ctx = canvas.getContext('2d');
+const shakeBtn = document.querySelector('.shake');
 const MOVE_AMOUNT = 10;
 
 const width = canvas.width;
@@ -9,8 +9,8 @@ let x = Math.floor(Math.random() * width);
 let y = Math.floor(Math.random() * height);
 let hue = 0;
 
-ctx.lineJoin = "round";
-ctx.lineCap = "round";
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
 ctx.lineWidth = 10;
 ctx.strokeStyle = `hsl(${hue}, 60%, 60%)`;
 
@@ -25,17 +25,17 @@ function draw(options) {
   ctx.beginPath();
   ctx.moveTo(x, y);
   switch (options.key) {
-    case "ArrowUp":
-      y = y - MOVE_AMOUNT;
+    case 'ArrowUp':
+      y = y - MOVE_AMOUNT >= 0 ? y - MOVE_AMOUNT : 0;
       break;
-    case "ArrowDown":
-      y = y + MOVE_AMOUNT;
+    case 'ArrowDown':
+      y = y + MOVE_AMOUNT <= 1000 ? y + MOVE_AMOUNT : 1000;
       break;
-    case "ArrowLeft":
-      x = x - MOVE_AMOUNT;
+    case 'ArrowLeft':
+      x = x - MOVE_AMOUNT >= 0 ? x - MOVE_AMOUNT : 0;
       break;
-    case "ArrowRight":
-      x = x + MOVE_AMOUNT;
+    case 'ArrowRight':
+      x = x + MOVE_AMOUNT <= 1600 ? x + MOVE_AMOUNT : 1600;
       break;
     default:
       break;
@@ -43,27 +43,29 @@ function draw(options) {
   ctx.lineTo(x, y);
   ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.stroke();
+
+  console.log(x, y);
 }
 
 function handleKey(e) {
-  if (e.key.includes("Arrow")) {
+  if (e.key.includes('Arrow')) {
     e.preventDefault();
     draw({ key: e.key });
   }
 }
 
-window.addEventListener("keydown", handleKey);
+window.addEventListener('keydown', handleKey);
 
 function clearCanvas() {
   ctx.clearRect(0, 0, height, width);
-  canvas.classList.add("shake");
+  canvas.classList.add('shake');
   canvas.addEventListener(
-    "animationend",
-    function() {
-      canvas.classList.remove("shake");
+    'animationend',
+    function () {
+      canvas.classList.remove('shake');
     },
     { once: true }
   );
 }
 
-shakeBtn.addEventListener("click", clearCanvas);
+shakeBtn.addEventListener('click', clearCanvas);
