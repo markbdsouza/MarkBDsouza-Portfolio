110e96c97ca7715845d9e36997db66cb1c51e382
Mark B DSouza <mark.benjamin.dsouza@gmail.com>
2020-06-05 00:55:40 +0530

    repeat mini projects with JS

--- scripts/miniProjectList.js
+++ scripts/miniProjectList.js
@@ -3,54 +3,52 @@ export const LIST_OF_MINI_PROJECTS = [
     fontAwesomeIcon: `fab fa-wpforms`,
     title: `Form Validator`,
     folderName: `Form Validator`,
-    description: `A Basic Registeration Form that ask's the user for
-      a username, email, password and re-enter password. This implements some basic form validation
-      such as mandatory checks, password checks and deals with basic DOM manipulation`,
+    description: `A Basic Registeration Form that ask's the user for a username, email, password and re-enter password. This implements some basic form validation as mandatory checks, password checks and deals with basic DOM manipulation`,
     credit: `Part of Brad Traversy's JavaScript Course`,
   },
   {
-    fontAwesomeIcon: `fab fa-wpforms`,
-    title: `Form Validator`,
-    folderName: `Form Validator`,
-    description: `A Basic Registeration Form that ask's the user for
-      a username, email, password and re-enter password. This implements some basic form validation
-      such as mandatory checks, password checks and deals with basic DOM manipulation`,
+    fontAwesomeIcon: `fas fa-pencil-ruler`,
+    title: `Etch a Sketch`,
+    folderName: `Etch a Sketch`,
+    description: `I still remember Joey and Chandler having one of these. This is an online etch a sketch built using the canvas element and basic ctx commands - beginPath, moveTo, lineTo, stroke. Also used keyframes to mimic a shake`,
+    credit: `Part of Wes Bos Beginner JS Program`,
+  },
+  {
+    fontAwesomeIcon: `fas fa-film`,
+    title: `Movie Seat Selector`,
+    folderName: `Movie-Seat-Booking`,
+    description: `A very simple Movie Seat Booking application which handles basic events of handling drop down values and click events which would update DOM elements. Additionally, thisalso contains using the local cache to store the selected movie and seats`,
     credit: `Part of Brad Traversy's JavaScript Course`,
   },
   {
-    fontAwesomeIcon: `fab fa-wpforms`,
-    title: `Form Validator`,
-    folderName: `Form Validator`,
-    description: `A Basic Registeration Form that ask's the user for
-  a username, email, password and re-enter password. This implements some basic form validation
-  such as mandatory checks, password checks and deals with basic DOM manipulation`,
+    fontAwesomeIcon: `fab fa-youtube`,
+    title: `Customer Video Player`,
+    folderName: `Project 3 HTML5 Video API`,
+    description: `This was a fun way of exploring the APIs coming in with the video element
+    which
+    included stop, pause, play & even a nice slider tracking the time which can be moved around as
+    well`,
     credit: `Part of Brad Traversy's JavaScript Course`,
   },
   {
-    fontAwesomeIcon: `fab fa-wpforms`,
-    title: `Form Validator`,
-    folderName: `Form Validator`,
-    description: `A Basic Registeration Form that ask's the user for
-  a username, email, password and re-enter password. This implements some basic form validation
-  such as mandatory checks, password checks and deals with basic DOM manipulation`,
+    fontAwesomeIcon: `far fa-money-bill-alt`,
+    title: `Exchange Rate Calculator`,
+    folderName: `Exchange Rate Calculator`,
+    description: ` A simple exchange rate calculator which made use of the api.exchangeratesapi.io to convert currency from one to another. This was doing using async await.`,
     credit: `Part of Brad Traversy's JavaScript Course`,
   },
   {
-    fontAwesomeIcon: `fab fa-wpforms`,
-    title: `Form Validator`,
-    folderName: `Form Validator`,
-    description: `A Basic Registeration Form that ask's the user for
-  a username, email, password and re-enter password. This implements some basic form validation
-  such as mandatory checks, password checks and deals with basic DOM manipulation`,
+    fontAwesomeIcon: `fas fa-layer-group`,
+    title: `Array Methods Excercise`,
+    folderName: `Array Methods`,
+    description: `Not the most useful of apps, but this code basically helps the coder understand the popular Array methods used in JavaScript. This also uses Async await when fetching a random user from the randomuser.me API. The methods implemented are map, sort, filter & reduce`,
     credit: `Part of Brad Traversy's JavaScript Course`,
   },
   {
-    fontAwesomeIcon: `fab fa-wpforms`,
-    title: `Form Validator`,
-    folderName: `Form Validator`,
-    description: `A Basic Registeration Form that ask's the user for
-  a username, email, password and re-enter password. This implements some basic form validation
-  such as mandatory checks, password checks and deals with basic DOM manipulation`,
-    credit: `Part of Brad Traversy's JavaScript Course`,
+    fontAwesomeIcon: `fas fa-gamepad`,
+    title: `Pokedex`,
+    folderName: `Pokedex`,
+    description: `This was a really fun simple project using the https://pokeapi.co/api/v2 API to get information about the first gen Pokemon. The rest is just DOM manipulation and pretty cool styling`,
+    credit: `Part of Florin Pop's Youtube series`,
   },
 ];
