5475b917c376e3c8c0f0b8cb175962a3a09b84f4
Mark B DSouza <mark.benjamin.dsouza@gmail.com>
2020-06-07 23:40:41 +0530

    teacher section

--- scripts/miniProjectList.js
+++ scripts/miniProjectList.js
@@ -3,28 +3,28 @@ export const LIST_OF_MINI_PROJECTS = [
     fontAwesomeIcon: `fab fa-wpforms`,
     title: `Form Validator`,
     folderName: `Form Validator`,
-    description: `A Basic Registeration Form that ask's the user for a username, email, password and re-enter password. This implements some basic form validation as mandatory checks, password checks and deals with basic DOM manipulation`,
+    description: `A Basic Registeration Form that ask's the user for a username, email, password and re-enter password. This implements some basic <strong>Form Validation</strong> as mandatory checks, password checks and deals with basic DOM manipulation`,
     credit: `Part of Brad Traversy's JavaScript Course`,
   },
   {
     fontAwesomeIcon: `fas fa-pencil-ruler`,
     title: `Etch a Sketch`,
     folderName: `Etch a Sketch`,
-    description: `I still remember Joey and Chandler having one of these. This is an online etch a sketch built using the canvas element and basic ctx commands - beginPath, moveTo, lineTo, stroke. Also used keyframes to mimic a shake`,
+    description: `I still remember Joey and Chandler having one of these. This is an online etch a sketch built using the <strong>Canvas Element</strong> and basic ctx commands - beginPath, moveTo, lineTo, stroke. Also used keyframes to mimic a shake`,
     credit: `Part of Wes Bos Beginner JS Program`,
   },
   {
     fontAwesomeIcon: `fas fa-film`,
     title: `Movie Seat Selector`,
     folderName: `Movie-Seat-Booking`,
-    description: `A very simple Movie Seat Booking application which handles basic events of handling drop down values and click events which would update DOM elements. Additionally, thisalso contains using the local cache to store the selected movie and seats`,
+    description: `A very simple Movie Seat Booking application which implements basic <strong>Event Handling</strong> on changes in drop down values and click events which would update DOM elements. Additionally, this also contains using the <strong>Local Storage</strong> to temporarily save the selected movie and seats`,
     credit: `Part of Brad Traversy's JavaScript Course`,
   },
   {
     fontAwesomeIcon: `fab fa-youtube`,
     title: `Customer Video Player`,
     folderName: `HTML5 Video API`,
-    description: `This was a fun way of exploring the APIs coming in with the video element
+    description: `This was a fun way of exploring the APIs coming in with the <strong>Video Element</strong>
     which
     included stop, pause, play & even a nice slider tracking the time which can be moved around as
     well`,
@@ -34,14 +34,14 @@ export const LIST_OF_MINI_PROJECTS = [
     fontAwesomeIcon: `far fa-money-bill-alt`,
     title: `Exchange Rate Calculator`,
     folderName: `Exchange Rate Calculator`,
-    description: ` A simple exchange rate calculator which made use of the api.exchangeratesapi.io to convert currency from one to another. This was doing using async await.`,
+    description: ` A simple exchange rate calculator which made use of the api.exchangeratesapi.io to convert one currency to another. This was doing using <strong>Async Await</strong>`,
     credit: `Part of Brad Traversy's JavaScript Course`,
   },
   {
     fontAwesomeIcon: `fas fa-layer-group`,
     title: `Array Methods Excercise`,
     folderName: `Array Methods`,
-    description: `Not the most useful of apps, but this code basically helps the coder understand the popular Array methods used in JavaScript. This also uses Async await when fetching a random user from the randomuser.me API. The methods implemented are map, sort, filter & reduce`,
+    description: `Not the most useful of apps, but this code basically helps the coder understand the popular <strong>Array methods</strong> used in JavaScript. This also uses Async await when fetching a random user from the randomuser.me API. The methods implemented are <strong>map, sort, filter & reduce</strong>`,
     credit: `Part of Brad Traversy's JavaScript Course`,
   },
   {
@@ -50,19 +50,20 @@ export const LIST_OF_MINI_PROJECTS = [
     folderName: `Pokedex`,
     description: `This was a really fun simple project using the https://pokeapi.co/api/v2 API to get information about the first gen Pokemon. The rest is just DOM manipulation and pretty cool styling`,
     credit: `Part of Florin Pop's Youtube series`,
+    highlight: true,
   },
   {
     fontAwesomeIcon: `fas fa-angle-right`,
     title: `Menu Slider and Modal Pop-up`,
     folderName: `Menu Slider and Modal`,
-    description: `A simple but very realtime sloution implementing 2 common usecases in a website - a menu that is disaplyed as button & that slides from the left pane. And a Sign up Modal which darkens the background and comes as a popup. This also uses a CSS keyframe for some animation.`,
+    description: `A simple solution implementing 2 common usecases - a menu that is displayed as a button & slides from the left pane. And a Sign up Modal which darkens the background and comes as a <strong>Popup</strong>. This also uses a CSS keyframe for some animation.`,
     credit: `Part of Brad Traversy's JavaScript Course`,
   },
   {
     fontAwesomeIcon: `fas fa-male`,
     title: `HangMan (using SVG)`,
     folderName: `HangMan with SVG`,
-    description: `A simple HangMan game that makes use of SVG to draw the hangman with each wrong letter that is prompted by the user. The user needs to type in the keyboard and those events are read and accordingly actions are done on the screen`,
+    description: `A simple HangMan game that makes use of <strong>SVG</strong> to draw the hangman with each wrong letter that is prompted by the user. The user needs to type in the keyboard and those events are read and accordingly actions are done on the screen`,
     credit: `Part of Brad Traversy's JavaScript Course`,
   },
   {
@@ -77,7 +78,7 @@ export const LIST_OF_MINI_PROJECTS = [
     fontAwesomeIcon: `fas fa-tags`,
     title: `A page with tabs`,
     folderName: `Tabs`,
-    description: `Implementing a simple Tab functionality`,
+    description: `Implementing a simple <strong>Tab</strong> functionality on a webpage`,
     credit: `Part of Wes Bos Beginner JS Program`,
   },
 ];
--- scripts/script.js
+++ scripts/script.js
@@ -2,18 +2,19 @@ import { LIST_OF_MINI_PROJECTS } from './miniProjectList.js';
 
 let cardsHolder = document.querySelector('.container.card-columns');
 
-console.log(cardsHolder);
-
 function addCardToHolder(card) {
-  let cardHTML = `<div class="card p-1">
-    <div class="card-body">
+  let cardHTML = `<div class="card ">
+    <div class="card-body ${card.highlight ? `highlight` : ``}">
         <div class="card-title h4">
             <i class="${card.fontAwesomeIcon} fa-2x pb-2"></i>
-            <a href="./Mini-Projects/${card.folderName}/index.html" class="">${card.title}</a>
+            <a href="./Mini-Projects/${card.folderName}/index.html" class="">${
+    card.title
+  }</a>
         </div>
         <div class="card-text"> ${card.description}</div>
-        <div class="text-muted"> ${card.credit}</div>
+        
         </div>
+        <div class="text-muted card-footer"> ${card.credit}</div>
     </div>`;
   cardsHolder.insertAdjacentHTML('beforeend', cardHTML);
 }
